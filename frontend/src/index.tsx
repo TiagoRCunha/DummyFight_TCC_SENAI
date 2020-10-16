6d5aaf4e7ba1ad762fbf458fccc6bab94bebe015
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-10-16 10:30:20 -0300

    Resolve webpack config

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,38 @@
+import { MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './modules/App';
+import { AppContainer } from 'react-hot-loader';
+import { Router } from 'react-router-dom';
+import history from './config/history';
+import App from './pages/App';
+import theme from "./theme";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root')
+
+const renderApp = (Application: typeof App) => {
+
+  ReactDOM.render(
+    <AppContainer>
+      <React.StrictMode>
+        <Router history={history}>
+          <MuiThemeProvider theme={theme}>
+            <Application />
+          </MuiThemeProvider>
+        </Router>
+      </React.StrictMode>
+    </AppContainer>
+    ,
+    root
+  );
+}
+
+renderApp(App)
+
+
+/* if (module.hot) {
+  module.hot.accept('./pages/App', () => {
+
+    const NextApp = require('./pages/App').default;
+    renderApp(NextApp);
+  });
+} */
---
+++ frontend/webpack.config.js
@@ -0,0 +1,69 @@
+const { resolve, join } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin')
+
+const ROOT = resolve(__dirname)
+
+module.exports = {
+  context: resolve(ROOT, 'src'),
+  entry: {
+    app: [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      resolve(ROOT, './src/index.tsx')
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: resolve(ROOT, '/dist'),
+    publicPath: resolve(ROOT, '/public')
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    hot: true,
+    contentBase: join(ROOT, '/public'),
+    port: 3000,
+    disableHostCheck: true,
+    compress: false,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'cache-loader',
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: require('os').cpus().length - 1
+            }
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true,
+              configFile: resolve(ROOT, 'tsconfig.json')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement'
+    }),
+    new ForkTsCheckerPlugin({
+      checkSyntacticErrors: false,
+      tsconfig: resolve(ROOT, 'tsconfig.json')
+    })
+  ]
+}
