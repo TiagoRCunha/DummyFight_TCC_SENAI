585b9f0d8cfe464d2554690f657510df94283b82
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-10-17 14:44:01 -0300

    Test module User

---
+++ frontend/src/modules/domain/index.ts
@@ -0,0 +1,7 @@
+/*
+ * This directory will serve as a model for the infrastructure layer,
+ * the entities will have values ​​that are only of interest to the frontend
+ *
+ * @author Tiago Rodrigues Cunha
+ *
+ */
